899f19cc1565b8911b5f333649482176fc708087
AmroBaniIssa <amro.nedal.issa@gmail.com>
2023-04-17 06:10:42 +0000

    last Table

--- accounting.js
+++ accounting.js
@@ -1,86 +1,149 @@
 "use strict ";
+
 let main = document.getElementById("main");
-let myForm = document.getElementById("employeesForm");
 let emolooyesInfo = document.getElementById("emolooyesInfo");
-const employees = [];
-function Employee(Department, numberOfEmployees, avgSalary) {
-  this.Department = Department;
-  this.numberOfEmployees = numberOfEmployees;
-  this.avgSalary = avgSalary;
-  employees.push(this);
+const employeesTableArray = [];
+
+function getData() {
+  let retrievedData = localStorage.getItem("employees");
+  let arrayData = JSON.parse(retrievedData);
+  if (arrayData != null) {
+    for (let i = 0; i < arrayData.length; i++) {
+         employeesTableArray.push(arrayData[i]);
+    }
+   
+  }
+  tableRender();
+  return employeesTableArray;
+}
+console.log(employeesTableArray);
+
+// ====== Render the table======
+// ===============================
+
+function tableRender() {
+  let numberOfAdministrEmplo = 0;
+  let numberOfMarkitingEmplo = 0;
+  let numberOfDevEmplo = 0;
+  let numberOfFinanceEmplo = 0;
+  let adminAvg = 0;
+  let markitingAvg = 0;
+  let devAvg = 0;
+  let financeAvg = 0;
+  for (let i = 0; i < employeesTableArray.length; i++) {
+    if (employeesTableArray[i].Department === "Administration") {
+      numberOfAdministrEmplo += 1;
+      adminAvg += employeesTableArray[i].Salary;
+    }
+    if (employeesTableArray[i].Department === "Marketing") {
+      numberOfMarkitingEmplo += 1;
+      markitingAvg += employeesTableArray[i].Salary;
+    }
+    if (employeesTableArray[i].Department === "Finance") {
+      numberOfFinanceEmplo += 1;
+      financeAvg += employeesTableArray[i].Salary;
+    }
+    if (employeesTableArray[i].Department === "Development") {
+      numberOfDevEmplo += 1;
+      devAvg += employeesTableArray[i].Salary;
+    }
+  }
+
+  if (numberOfAdministrEmplo != 0) {
+    let tableRow1 = document.createElement("tr");
+    emolooyesInfo.appendChild(tableRow1);
+    let debartmentNameAdmin = document.createElement("td");
+    debartmentNameAdmin.textContent = "Adminstration";
+    let numberOfAdministrEmploValu = document.createElement("td");
+    numberOfAdministrEmploValu.textContent = numberOfAdministrEmplo;
+    console.log(numberOfAdministrEmplo);
+    let avgSalaryAdmin = document.createElement("td");
+    avgSalaryAdmin.textContent = adminAvg / numberOfAdministrEmplo;
+    let debartmentTotalAdmin = document.createElement("td");
+    debartmentTotalAdmin.textContent = adminAvg;
+    tableRow1.appendChild(debartmentNameAdmin);
+    tableRow1.appendChild(numberOfAdministrEmploValu);
+    tableRow1.appendChild(avgSalaryAdmin);
+    tableRow1.appendChild(debartmentTotalAdmin);
+  }
+
+  if (numberOfMarkitingEmplo != 0) {
+    let tableRow1 = document.createElement("tr");
+    emolooyesInfo.appendChild(tableRow1);
+    let debartmentNameAdmin = document.createElement("td");
+    debartmentNameAdmin.textContent = "Markiting";
+    let numberOfAdministrEmploValu = document.createElement("td");
+    numberOfAdministrEmploValu.textContent = numberOfMarkitingEmplo;
+    let avgSalaryAdmin = document.createElement("td");
+    avgSalaryAdmin.textContent = markitingAvg / numberOfMarkitingEmplo;
+    let debartmentTotalAdmin = document.createElement("td");
+    debartmentTotalAdmin.textContent = markitingAvg;
+    tableRow1.appendChild(debartmentNameAdmin);
+    tableRow1.appendChild(numberOfAdministrEmploValu);
+    tableRow1.appendChild(avgSalaryAdmin);
+    tableRow1.appendChild(debartmentTotalAdmin);
+  }
+
+  if (numberOfDevEmplo != 0) {
+    let tableRow1 = document.createElement("tr");
+    emolooyesInfo.appendChild(tableRow1);
+    let debartmentNameAdmin = document.createElement("td");
+    debartmentNameAdmin.textContent = "Development";
+    let numberOfAdministrEmploValu = document.createElement("td");
+    numberOfAdministrEmploValu.textContent = numberOfDevEmplo;
+    let avgSalaryAdmin = document.createElement("td");
+    avgSalaryAdmin.textContent = devAvg / numberOfDevEmplo;
+    let debartmentTotalAdmin = document.createElement("td");
+    debartmentTotalAdmin.textContent = devAvg;
+    tableRow1.appendChild(debartmentNameAdmin);
+    tableRow1.appendChild(numberOfAdministrEmploValu);
+    tableRow1.appendChild(avgSalaryAdmin);
+    tableRow1.appendChild(debartmentTotalAdmin);
+  }
+
+  if (numberOfFinanceEmplo != 0) {
+    let tableRow1 = document.createElement("tr");
+    emolooyesInfo.appendChild(tableRow1);
+    let debartmentNameAdmin = document.createElement("td");
+    debartmentNameAdmin.textContent = "Finance";
+    let numberOfAdministrEmploValu = document.createElement("td");
+    numberOfAdministrEmploValu.textContent = numberOfFinanceEmplo;
+    let avgSalaryAdmin = document.createElement("td");
+    avgSalaryAdmin.textContent = financeAvg / numberOfFinanceEmplo;
+    let debartmentTotalAdmin = document.createElement("td");
+    debartmentTotalAdmin.textContent = financeAvg;
+    tableRow1.appendChild(debartmentNameAdmin);
+    tableRow1.appendChild(numberOfAdministrEmploValu);
+    tableRow1.appendChild(avgSalaryAdmin);
+    tableRow1.appendChild(debartmentTotalAdmin);
+  }
+
+  if (numberOfFinanceEmplo != 0) {
+    let tableRow1 = document.createElement("tr");
+    emolooyesInfo.appendChild(tableRow1);
+    let debartmentNameAdmin = document.createElement("td");
+    debartmentNameAdmin.textContent = "Total";
+    let numberOfAdministrEmploValu = document.createElement("td");
+    numberOfAdministrEmploValu.textContent =
+      numberOfFinanceEmplo +
+      numberOfAdministrEmplo +
+      numberOfMarkitingEmplo +
+      numberOfDevEmplo;
+    let avgSalaryAdmin = document.createElement("td");
+    avgSalaryAdmin.textContent =
+      financeAvg / numberOfFinanceEmplo +
+      devAvg / numberOfDevEmplo +
+      markitingAvg / numberOfMarkitingEmplo +
+      adminAvg / numberOfAdministrEmplo;
+    let debartmentTotalAdmin = document.createElement("td");
+    debartmentTotalAdmin.textContent =
+      financeAvg + devAvg + markitingAvg + adminAvg;
+    tableRow1.appendChild(debartmentNameAdmin);
+    tableRow1.appendChild(numberOfAdministrEmploValu);
+    tableRow1.appendChild(avgSalaryAdmin);
+    tableRow1.appendChild(debartmentTotalAdmin);
+  }
 }
 
-// ==== adding event listener for the form ===
-///===================================
-myForm.addEventListener("submit", handelSubmit);
-
-function handelSubmit(event) {
-  event.preventDefault();
-  let Department = event.target.Department.value;
-  let numberOfEmployees = event.target.numberOfEmployees.value;
-  let avgSalary = event.target.avgSalary.value;
-
-  let newEmployee = new Employee(Department, numberOfEmployees, avgSalary);
-
-  newEmployee.TableRender();
-}
-
-//====== render prototype function ======
-// ==================
-let TotalNumber = 0;
-let avgSalaryOfAllDep = 0;
-Employee.prototype.TableRender = function () {
-  TotalNumber = TotalNumber + parseInt(this.numberOfEmployees);
-  avgSalaryOfAllDep = (avgSalaryOfAllDep + parseInt(this.avgSalary)) / 4;
-  let totalSalarys = avgSalaryOfAllDep * TotalNumber;
-
-  let tableRow = document.createElement("tr");
-  emolooyesInfo.appendChild(tableRow);
-
-  let debartmentName = document.createElement("td");
-  debartmentName.textContent = this.Department;
-  console.log(this);
-
-  tableRow.appendChild(debartmentName);
-  console.log(this.avgSalary);
-
-  let numberOfEmp = document.createElement("td");
-  numberOfEmp.textContent = this.numberOfEmployees;
-  tableRow.appendChild(numberOfEmp);
-
-  let avgSalary = document.createElement("td");
-  avgSalary.textContent = this.avgSalary;
-  tableRow.appendChild(avgSalary);
-
-  let totalSalary = document.createElement("td");
-  totalSalary.textContent = this.avgSalary * this.numberOfEmployees;
-  tableRow.appendChild(totalSalary);
-  //==================
-  //==================
-  let tableRow2 = document.createElement("tr");
-  emolooyesInfo.appendChild(tableRow2);
-
-  let TotalWord = document.createElement("td");
-  TotalWord.textContent = "total";
-  tableRow2.appendChild(TotalWord);
-  console.log(TotalWord.textContent);
-
-  let TotalNumberOfEmployees = document.createElement("td");
-  TotalNumberOfEmployees.textContent = TotalNumber;
-  tableRow2.appendChild(TotalNumberOfEmployees);
-
-  let avgSalaryOfDepartments = document.createElement("td");
-  avgSalaryOfDepartments.textContent = avgSalaryOfAllDep;
-  tableRow2.appendChild(avgSalaryOfDepartments);
-
-  let totalSalaryOfDepartments = document.createElement("td");
-  totalSalaryOfDepartments.textContent = totalSalarys;
-  tableRow2.appendChild(totalSalaryOfDepartments);
-};
-
-
-
-for (let i = 0; i < employees.length; i++) {
-  employees[i].render();
-}
-
+tableRender();
+getData();
